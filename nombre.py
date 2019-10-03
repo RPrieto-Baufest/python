e06be0d70c6372da915aae310b3d1484fe90d4f2
RPrieto-Baufest <rprieto@baufest.com>
2019-10-03 16:34:56 -0300

    segundo commit

---
+++ nombre.py
@@ -0,0 +1,8 @@
+__author__ = 'Rodrigo Prieto'
+nombre = input("por favor ingrese su nombre: ")
+edad = int(input("ingrese su edad {0}:".format(nombre)))
+print(edad)
+if edad > 40:
+    print("estas viejo mierda")
+else:
+    print("todo bien")
