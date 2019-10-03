500cda44293a4ead197cb157a1e852dd80af8fe5
RPrieto-Baufest <rprieto@baufest.com>
2019-10-03 15:56:58 -0300

    primer commit

---
+++ listasYtuplas.py
@@ -0,0 +1,17 @@
+#una lista puede contener multiples tipos de datos
+#ej
+c = ["a",2,"hola"]
+# se indexa igual que los strings
+
+print(c[1])
+
+#conocer los metodos disponibles para las listas
+#dir(list)
+
+#con append puedo agregar elementos:
+c.append("mundo")
+print(c)
+# una tupla es como una lista pero inmutabel
+
+t = ("a",2,"z")
+print(t)
---
+++ pruebaEindex.py
@@ -0,0 +1,13 @@
+print("hola")
+c = "mundo del mundopython"
+print(c)
+#indexado, a cualquier string en python se le asigna un numero a cada elemento del stringp
+print(c[3])
+print(type(c[3]))
+
+#para indexar desde hacia atras , se empieza dede -1 (es el ultimo caracter del string)
+#definir rangos :
+print(c[-4])
+print(c[1:6])
+#atajo desde el cero
+print(c[:11ad])
